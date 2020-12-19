6609f33b0fcb968f865a398738e0378e0736c667
fs2050 <fabiolfs76@hotmail.com>
2020-12-19 00:47:10 -0300

    implementando pasta charts dentro de app

---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+const SAMPLE_BARCHART_DATA: any[] = [
+  { data:[65, 59, 80, 81, 56, 54, 30], label: 'Q3 sales'},
+  { data:[63, 69, 90, 81, 55, 74, 10], label: 'Q4 Sales'}
+];
+
+const SAMPLE_BARCHART_LABELS: string[] = [
+  'w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7'
+];
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.css']
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+  public barChartData: any[] = SAMPLE_BARCHART_DATA;
+  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  }
+
+
+
+  ngOnInit() {
+
+  }
+
+}
