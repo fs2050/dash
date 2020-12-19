98d2056da5c9b84a0cb841455878340d258bbe25
fs2050 <fabiolfs76@hotmail.com>
2020-12-19 00:52:23 -0300

    DashBorad Power BI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule} from '@angular/router';
 import {appRoutes} from '../routes';
 import { NgModule } from '@angular/core';
+import {ChartsModule} from 'ng2-charts';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +25,19 @@ import { SectionHealthComponent } from './sections/section-health/section-health
 
     SectionOrdersComponent,
 
-    SectionHealthComponent
+    SectionHealthComponent,
+
+    BarChartComponent,
+
+    LineChartComponent,
+
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
